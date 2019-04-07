f0f96b6a379597ecbd38c7d9022c965fd6e5777f
kiki_xie <kiki_xie@10.215.219.187>
2019-04-07 15:34:53 -0400

    change comments and method names

--- Wuzq/src/cn/lantian/Fivezq/WuzqFrame.java
+++ Wuzq/src/cn/lantian/Fivezq/WuzqFrame.java
@@ -11,36 +11,36 @@ public class WuzqFrame extends JFrame {
 	 * 
 	 * @兰天
 	 */
-	// 入口主函数
+	// this is the main method
 	public static void main(String[] args) {
-		WuzqFrame wzq = new WuzqFrame();
-		wzq.init();
+		WuzqFrame cf = new WuzqFrame();
+		cf.init();
 	}
 
 	public void init() {
-		chushi();// 初始化棋盘
-		// 基本设置
-		this.setTitle("五子棋");
+		initialize();// initialization
+		// basic settings
+		this.setTitle("Chess");
 		this.setSize(520, 535);
 		this.setDefaultCloseOperation(3);
-		// 设置居中
+		// center the interface
 		this.setLocationRelativeTo(null);
-		// 用户不可调整大小
+		// user cannot adjust the size of the chessboard
 		this.setResizable(false);
 		this.setVisible(true);
-		// 获取窗体的画布
+		// get the canvas
 		Graphics g = this.getGraphics();
-		// 定义鼠标监听类
+		// new a mouselistener class
 		wzqListener wzql = new wzqListener(g, this);
-		// 添加鼠标监听
+		// use mouse listener method
 		this.addMouseListener(wzql);
 	}
 
-	// 对窗体进行重绘
+	// paint chessboard method
 	public void paint(Graphics g) {
-		// 调用父类的方法，super
+		// use super method
 		super.paint(g);
-		// 画棋盘
+		// draw the chessboard
 		for (int i = 0; i < n + 1; i++) {
 			g.drawLine(x + i * size, y, x + i * size, y + n * size);
 		}
@@ -53,25 +53,25 @@ public class WuzqFrame extends JFrame {
 					g.fillOval(x + i * WuzqFrame.size - WuzqFrame.size / 2, y + j * WuzqFrame.size - WuzqFrame.size / 2,
 							WuzqFrame.qsize, WuzqFrame.qsize);
 				} else if (s[i][j] == -1) {
-					// 将颜色改为白色
+					// change the color into white
 					g.setColor(Color.WHITE);
 					g.fillOval(x + i * WuzqFrame.size - WuzqFrame.size / 2, y + j * WuzqFrame.size - WuzqFrame.size / 2,
 							WuzqFrame.qsize, WuzqFrame.qsize);
-					// 定义黑色为初始色
+					// define the black color as default
 					g.setColor(Color.BLACK);
 				}
 
 	}
 
-	// 定义起始直线坐标
+	// define the coordinate of the starting point
 	public static int x = 30, y = 50;
-	// 棋子的多少及格子大小
+	// define the number of chess and the size of grids
 	public static int n = 15, size = 30, qsize = 30;
-	// 定义一个数组来存储棋子
+	// new an two dimensional array to store the chess
 	public static int[][] s = new int[n][n];
 
-	// 初始方法，数组全赋为0
-	public void chushi() {
+	// initialize method. set all numbers zero
+	public void initialize() {
 		for (int i = 0; i < n; i++)
 			for (int j = 0; j < n; j++)
 				s[i][j] = 0;
