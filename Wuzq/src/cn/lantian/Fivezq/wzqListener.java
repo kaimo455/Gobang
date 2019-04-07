774c85befd912c2fafa7781214555bea2f3fd3c4
Cecilia-xu <42839518+Cecilia-xu@users.noreply.github.com>
2019-04-07 15:42:23 -0400

    Translation comments in wzqListener.java

--- Wuzq/src/cn/lantian/Fivezq/wzqListener.java
+++ Wuzq/src/cn/lantian/Fivezq/wzqListener.java
@@ -4,32 +4,32 @@ import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-//棋盘鼠标监听器 
+//Listener for mouse action
 public class wzqListener implements MouseListener {
 	private Graphics g;
-	// 用来确定下什么棋
+	// Initialization
 	public static int t = 1;
 	private WuzqFrame wzq;
 
-	// 构造器，将画布传过来
+	// Chess graph
 	public wzqListener(Graphics g, WuzqFrame wzq) {
 		this.g = g;
 		this.wzq = wzq;
 	}
 
 	public void mousePressed(MouseEvent e) {
-		// 获得要下的棋子的行和列
+		// get the coordinate of the chess
 		int x1 = Math.round((float) (e.getX() - WuzqFrame.x) / WuzqFrame.size);
 		int y1 = Math.round((float) (e.getY() - WuzqFrame.y) / WuzqFrame.size);
-		// 判断行列是否出界，及是否放了棋子
+		// determine whether other chess locates on this coordinate or whether the coordinate is out of bound
 		if (x1 >= 0 && x1 < WuzqFrame.n && y1 >= 0 && y1 < WuzqFrame.n && WuzqFrame.s[x1][y1] == 0) {
-			// 棋子放在棋盘上后，数组做相应标示
+			// identify the coordinate of chess on the chess graph 
 			WuzqFrame.s[x1][y1] = t;
-			// 重绘棋盘
+			// repaint the chess graph
 			wzq.repaint();
-			// 调用QiziPd类中的方法，来判断输赢
+			// decide whether any player win the game
 			QiziPd.pd(t, x1, y1, g, wzq);
-			// 改变棋子颜色
+			// change the color of the chess
 			t = -t;
 		}
 	}
