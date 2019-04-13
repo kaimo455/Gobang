cbe96983e14308fc5b6143a2f01da903a7b9795c
kiki_xie <kiki_xie@10.215.210.26>
2019-04-13 12:34:57 -0400

    change board color

--- New-gobang-master/src/algorithm_design/gobang/App.java
+++ New-gobang-master/src/algorithm_design/gobang/App.java
@@ -9,11 +9,12 @@ public class App {
 		// basic setting for this chess frame 
 		chessframe.setTitle("Gobang");
 		chessframe.setSize(480, 500);
+		chessframe.getContentPane().setBackground(new Color(248,230,150));
 		chessframe.setDefaultCloseOperation(3);
 		chessframe.setLocationRelativeTo(null);
 		chessframe.setResizable(false);
 		chessframe.setVisible(true);
-	//	chessframe.getContentPane().setBackground(new Color(248,230,150));
+		
 		chessframe.init();
 		// mouse listening class instance
 		MouseListener mouselistener = new MouseListener(chessframe);
