41e2a9002ba22525780b69502f7396d26387b834
kiki_xie <kiki_xie@10.215.215.240>
2019-04-14 13:39:20 -0400

    version 1.0 new images

--- New-gobang-master/src/algorithm_design/gobang/App.java
+++ New-gobang-master/src/algorithm_design/gobang/App.java
@@ -1,19 +1,27 @@
 package algorithm_design.gobang;
-
+import javax.swing.*;
 import java.awt.Color;
 
 public class App {
+	public ImageIcon icon1;
+	public ImageIcon icon2;
+	
+	public App(){
+		icon1 = new ImageIcon("./data/lose.jpg");
+		icon2 = new ImageIcon("./data/win.jpg");
+	}
 
 	public static void main(String[] args) {
 		ChessFrame chessframe = new ChessFrame();
 		// basic setting for this chess frame 
 		chessframe.setTitle("Gobang");
 		chessframe.setSize(480, 500);
+		chessframe.getContentPane().setBackground(new Color(248, 230, 150));
 		chessframe.setDefaultCloseOperation(3);
 		chessframe.setLocationRelativeTo(null);
 		chessframe.setResizable(false);
 		chessframe.setVisible(true);
-//		chessframe.getContentPane().setBackground(new Color(248, 230, 150));
+		
 		chessframe.init();
 		// mouse listening class instance
 		MouseListener mouselistener = new MouseListener(chessframe);
--- New-gobang-master/src/algorithm_design/gobang/MouseListener.java
+++ New-gobang-master/src/algorithm_design/gobang/MouseListener.java
@@ -38,7 +38,8 @@ public class MouseListener implements java.awt.event.MouseListener {
 				this.chessframe.repaint();
 				if (this.chessframe.isEnd(x, y, ChessFrame.BLACK)) {
 					System.out.println("player wins!");
-					JOptionPane.showMessageDialog(null, "Congratulations,You Win!");
+					App app= new App();
+					JOptionPane.showMessageDialog(null, "Congratulations,You Win!","",JOptionPane.INFORMATION_MESSAGE,app.icon2);
 					this.chessframe.init();
 					this.chessframe.repaint();
 				} else {
@@ -57,7 +58,9 @@ public class MouseListener implements java.awt.event.MouseListener {
 			this.chessframe.makeMove(robotXY[0], robotXY[1], ChessFrame.WHITE);
 			if (this.chessframe.isEnd(robotXY[0], robotXY[1], ChessFrame.WHITE)) {
 				System.out.println("robot wins!");
-				JOptionPane.showMessageDialog(null, "Sorry, you lose.");
+				App app = new App();
+				JOptionPane.showMessageDialog(null, "Sorry, you lose.","", JOptionPane.INFORMATION_MESSAGE,app.icon1);
+				
 				this.chessframe.init();
 				this.chessframe.repaint();
 			}
